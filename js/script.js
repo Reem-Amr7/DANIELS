7a78b7df8dec4c3febe04ab854cb4c199d51ce3d
Reem-Amr7 <147060181+Reem-Amr7@users.noreply.github.com>
2024-11-27 21:54:22 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var typed = new Typed('#element', {
+    strings: [
+      'Designer',
+      'Larry Daniels',
+      'Freelancer',
+    ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: true, 
+    cursorChar: '|',  
+});
+
+
+
+
+
+
